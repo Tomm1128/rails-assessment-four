0888a3488946dd95c695a81393a15ac38b7dbaff
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-22 10:02:27 -0400

    Update routes and add nested route for new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :rooms, only: [:index, :show]
-  resources :house_plants, only: [:index, :show]
+  resources :rooms, only: [:index, :show] do
+    resources :house_plants, only: [:new]
+  end
+  resources :house_plants, only: [:index, :show, :create]
 end
