1b6f72bb8685c3ddc8170ca0a70a693702592f72
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-22 10:20:55 -0400

    Create new/create actions for house plant

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -7,4 +7,26 @@ class HousePlantsController < ApplicationController
     @house_plant = HousePlant.find(params[:id])
     @room = @house_plant.room
   end
+
+  def new
+    @room = Room.find(params[:room_id])
+    @plant = @room.house_plants.build
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @plant = @room.house_plants.build(house_plant_params)
+
+    if @plant.save
+      redirect_to @room
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def house_plant_params
+    params.require(:house_plant).permit(:plant_type, :height)
+  end
 end
